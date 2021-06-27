71f3c83bd3ac4cf53b1e182f8f5cfe13c11a2abe
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-06-27 06:48:16 -0800

    Initial

---
+++ BankInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
